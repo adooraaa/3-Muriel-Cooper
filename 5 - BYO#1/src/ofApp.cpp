d7610671c5c13671f29aff4ef4af9274f7683a2a
adooraaa <35205251+adooraaa@users.noreply.github.com>
2020-10-05 11:41:48 -0400

    Update ofApp.cpp

--- 5 - BYO#1/src/ofApp.cpp
+++ 5 - BYO#1/src/ofApp.cpp
@@ -13,21 +13,19 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     
-    ofBackground(255);
-   // ofSetColor(0);
-   
-    // --------- amplitude ---------
+       ofBackground(255);
+
         for (int i = 16; i < 80; i++){
-            float amt = ofMap(i, 0, 80, 0, 400);
+            float amt = ofMap(i, 0, 80, 0, 400);                    // amplitude from sine
             float x = 500 + ofMap(sin(i*0.2), -1, 1, -amt, amt);
-            ofDrawCircle(x,(i* mouseY), 0);
+            ofDrawCircle(x,(i* mouseY), 0);                         // original code from Zack's example, kept circles but hid with radius zero
             
-            ofSetColor(127 + 127 * sin(i*0.01));
-    ofNoFill();
-            ofSetLineWidth(10);
+            ofSetColor(127 + 127 * sin(i*0.01));                    // gradient, want it to be applied to curves
+            ofNoFill();
+            ofSetLineWidth(10);                                      // thicc lines
            
             
-            ofDrawCurve(260, 260, 500, 900, x, (i *(0.05)* mouseY)+900, 500, mouseY*100);
+            ofDrawCurve(260, 260, 500, 900, x, (i *(0.05)* mouseY)+900, 500, mouseY*100);  // used ofDrawCurve but would like more control, seeking way to draw curve with three points
          //   ofDrawCurve(0, 0, 400, 800, x, 800, 600, ((i/2)*0.1* mouseY));
             
             ofSetCurveResolution(200);
